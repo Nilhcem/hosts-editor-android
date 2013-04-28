9610eeeeaa4dc8313b5f314dc9e5eb2df8bed6c2
Gautier Nilhcem <nilhcem@gmail.com>
2013-04-28 19:06:48 +0200

    Add typefaced widgets (textview + button)

---
+++ src/com/nilhcem/hostseditor/widgets/TypefacedButton.java
@@ -0,0 +1,18 @@
+package com.nilhcem.hostseditor.widgets;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.Button;
+
+/**
+ * Button using a custom font.
+ *
+ * @see TypefacedTextView
+ */
+public final class TypefacedButton extends Button {
+
+	public TypefacedButton(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		TypefacedTextView.applyFont(context, attrs, this);
+	}
+}
---
+++ src/com/nilhcem/hostseditor/widgets/TypefacedTextView.java
@@ -0,0 +1,39 @@
+package com.nilhcem.hostseditor.widgets;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+import com.nilhcem.hostseditor.R;
+
+/**
+ * TextView using a custom font.
+ *
+ * @see {@link http://stackoverflow.com/questions/4395309/android-want-to-set-custom-fonts-for-whole-application-not-runtime/9199258#9199258}
+ */
+public final class TypefacedTextView extends TextView {
+
+	public TypefacedTextView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+
+		// Typeface.createFromAsset doesn't work in the layout editor. Skipping...
+		if (isInEditMode()) {
+			return;
+		}
+
+		TypefacedTextView.applyFont(context, attrs, this);
+	}
+
+	static void applyFont(Context context, AttributeSet attrs, TextView view) {
+		TypedArray styledAttrs = context.obtainStyledAttributes(attrs, R.styleable.TypefacedTextView);
+		String fontName = styledAttrs.getString(R.styleable.TypefacedTextView_typeface);
+		styledAttrs.recycle();
+
+		if (fontName != null) {
+			Typeface typeface = Typeface.createFromAsset(context.getAssets(), fontName);
+			view.setTypeface(typeface);
+		}
+	}
+}
