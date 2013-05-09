203db2cb92bffdfd685131bcf8beecd21347d425
Gautier Nilhcem <nilhcem@gmail.com>
2013-05-09 16:54:08 +0200

    Switch to error log level by default

--- src/com/nilhcem/hostseditor/util/Log.java
+++ src/com/nilhcem/hostseditor/util/Log.java
@@ -4,7 +4,7 @@ import java.util.IllegalFormatException;
 import java.util.Locale;
 
 public final class Log {
-	private static final int LOG_LEVEL = 4; // 1: error, 2: warn, 3: info, 4: debug
+	private static final int LOG_LEVEL = 1; // 1: error, 2: warn, 3: info, 4: debug
 
 	private static final boolean LOG_ERROR = LOG_LEVEL > 0;
 	private static final boolean LOG_WARN = LOG_LEVEL > 1;
